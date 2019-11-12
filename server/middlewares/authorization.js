1b6c8de04b2c9623ecc548aa0e257818d9c554b9
adilpratama <adilpratama71@yahoo.com>
2019-11-12 16:13:44 +0700

    add user auth & admin auth

--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -1,13 +1,22 @@
-const { Expense } = require('../models')
-module.exports = (req, res, next) => {
-  const authorizationMessage = {
-    status: 401,
-    message: "Unauthorized"
+const { Expense, User } = require('../models')
+const message = { status: 403, message: "Not Authorized" }
+module.exports = {
+  authorization: (req, res, next) => {
+    Expense.find({
+      _id: req.params.id,
+      UserId: req.decode._id
+    })
+    .then(doc => doc ? next() : next(message))
+    .catch(err => next(err))
+  },
+  userAuth: (req, res, next) => {
+    User.find({
+      _id: req.params.id
+    })
+    .then(doc => doc ? doc._id == req.decode._id ? next() : next(message) : next(message))
+    .catch(err => next(err))
+  },
+  adminAuth: (req, res, next) => {
+    req.decode.username == process.env.ADMIN_KEY ? next() : next(message) 
   }
-  Expense.find({
-    _id: req.params.id,
-    UserId: req.decode._id
-  })
-  .then(doc => doc ? next() : next(authorizationMessage))
-  .catch(err => next(err))
-} 
+}
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,11 @@
 const router = require('../config/express').Router()
 const { UserController } = require('../controllers')
+const { adminAuth, userAuth } = require('../middlewares/authorization')
 
 router.post('/', UserController.register)
 router.post('/login', UserController.login)
-router.put('/:id', UserController.update)
-router.get('/', UserController.findAll)
-router.delete('/all', UserController.temporaryDestroy)
+router.put('/:id', userAuth, UserController.update)
+router.get('/', adminAuth, UserController.findAll)
+router.delete('/all', adminAuth, UserController.temporaryDestroy)
 
 module.exports = router
